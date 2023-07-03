415d1400259207dade9e6cf33d30760d231f6ecc
Alcal <alcal.rs221@gmail.com>
2023-07-03 16:49:11 +0200

    Mise en place react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { Routes, Route } from "react-router-dom";
 import { Menu } from "./components/Menu";
 import './css/app.css';
 import { TodoAdd } from "./pages/TodoAdd";
 import { TodoList } from "./pages/TodoList";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <div>
       <Menu/>
-      <TodoAdd/>
-      <TodoList/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/add" element={<TodoAdd/>}/>
+        <Route path="/list" element={<TodoList/>}/>
+      </Routes>
+      
+      
+     
     </div>
   );
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,14 @@
+import { NavLink } from "react-router-dom"
 
-export function Menu(){
+export function Menu() {
 
-        return(
-            <div className="menu">
-                <ul>
-                    <li>Add</li>
-                    <li>List</li>
-                </ul>
-            </div>
-        )
+    return (
+        <div className="menu">
+            <ul>
+                <li><NavLink to="/" className={({ isActive }) => (isActive ? 'activeLink' : undefined)}>Home</NavLink></li>
+                <li><NavLink to="/add" className={({ isActive }) => (isActive ? 'activeLink' : undefined)}>Add</NavLink></li>
+                <li><NavLink to="/list" className={({ isActive }) => (isActive ? 'activeLink' : undefined)}>List</NavLink></li>
+            </ul>
+        </div>
+    )
 }
